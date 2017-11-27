b66594cf12158d0fabd2f026ce608fbcbb956fe1
Matt Thompson <matt@matteveryday.com>
2017-11-26 19:53:51 -0600

    updating jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
 
 
-
-$('h1 .playlist-title').hover(function(){
+// Changes the background color of the page
+$('h1 .playlist-title').hover(function () {
     
     var link = $(this)
     var overlay = link.data('overlay')
     
+    
     $('.overlay-bg').removeClass('visible')
     $(overlay).addClass('visible')
     
+    // Changes the text color of buttons to match the background
+    var bgColor = $(overlay).css("background-color")
+    console.log(bgColor)
+    
+    $('.button').css("color", bgColor)
     
 })
+
+// Keeps the buttons white when hovering over Surprise Me playlist
+$('#surprise-me').hover(function (){
+    
+    console.log("found it!")
+    $('.button').css("color", "white")
+    
+})
+
